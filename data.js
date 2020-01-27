d4ec8b0204480a4fc389d77da00593b68aaecc3c
Anusha <potluri.anusha1@gmail.com>
2020-01-27 07:26:37 +0530

    ecommerce website

---
+++ data.js
@@ -0,0 +1,10 @@
+ var shoppingData= [
+    {
+        name:"sandisk Hard disk",
+        company:"sandisk",
+        price: 22,
+        summary: "this is a hard disk",
+        description: " this is a hard disk",
+        image: "sandisk-hardisk.png"
+    }
+]
